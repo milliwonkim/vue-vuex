59583c45352a2c4a2ea1e6c47e05f4082a338eca
milliwonkim <rldnjs9347@gmail.com>
2020-07-14 01:12:40 +0900

    first commit

---
+++ src/store/store.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import counter from './modules/counter';
+
+import actions from './actions';
+import getters from './getters';
+import mutations from './mutations';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    /**'state' is passed automatically by vue
+     * when this function is executed by vuex
+     */
+    state: {
+        value: 0
+    },
+    getters,
+    mutations,
+    actions,
+    modules: {
+        counter
+    }
+});
